20c48174e5e4bed3b7177968c1b1a8c80f944547
ebezmenov <ebezmenov@gmail.com>
2010-05-28 23:14:48 +0300

    create view

--- gin/models.py
+++ gin/models.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length=50)
+    body = models.TextField()
+    timestamp = models.DateTimeField()
 
-admin.site.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp')
+
+admin.site.register(BlogPost, BlogPostAdmin)
