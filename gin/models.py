0e5622345d491344f68b3b524e5142be0e0fe1de
ebezmenov <ebezmenov@gmail.com>
2010-05-30 01:25:13 +0300

    add models for HCB

--- gin/models.py
+++ gin/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Create your models here.
 class BlogPost(models.Model):
@@ -10,6 +11,31 @@ class BlogPost(models.Model):
         ordering = ('-timestamp',)
 
 class BlogPostAdmin(admin.ModelAdmin):
+    """Class for me"""
     list_display = ('title', 'timestamp')
 
 admin.site.register(BlogPost, BlogPostAdmin)
+
+class EmployeeProfile(models.Model):
+    user = models.ForeignKey(User, unique = True)
+    departament = models.CharField(max_length=50)
+class EmployeeProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'departament')
+admin.site.register(EmployeeProfile,EmployeeProfileAdmin)
+
+class Client(models.Model):
+    first = models.CharField(max_length=50)
+    last = models.CharField(max_length=50)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first','last')
+admin.site.register(Client, ClientAdmin)
+
+class Incident(models.Model):
+    timestamp = models.DateField()
+    title = models.CharField(max_length=50)
+    client = models.ForeignKey(Client)
+    author = models.ForeignKey(User)
+    responsibles = models.ManyToManyField(User, related_name='respons')
+
+
+admin.site.register(Incident)
